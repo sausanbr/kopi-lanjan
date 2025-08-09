c044222dda3a60b53c1845e1ce8770f5efce7b7d
sausanbr <sausanberliana@gmail.com>
2025-08-09 23:53:56 +0700

    contentful cms scripts

---
+++ admin/cms-init.js
@@ -0,0 +1,25 @@
+CMS.init({
+  config: {
+    backend: {
+      name: "contentful",
+      space_id: CONTENTFUL_SPACE_ID,
+      access_token: CONTENTFUL_ACCESS_TOKEN,
+      environment: "master"
+    },
+    media_folder: "static/uploads",
+    public_folder: "/uploads",
+    collections: [
+      {
+        name: "artikel-desa",
+        label: "Artikel Desa",
+        fields: [
+          { label: "Judul", name: "judul", widget: "string" },
+          { label: "Tanggal Upload", name: "tanggalUpload", widget: "datetime" },
+          { label: "Penulis", name: "penulis", widget: "string" },
+          { label: "Foto", name: "foto", widget: "image" },
+          { label: "Isi Artikel", name: "isiArtikel", widget: "markdown" }
+        ]
+      }
+    ]
+  }
+});
